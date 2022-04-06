494fce577f94e7b0a9b8030a5c6ad954dcca8fd5
kimberlythomas <thomas.kimberlym@gmail.com>
2022-04-06 16:45:47 -0700

    Now generates random image with button click

--- js/script.js
+++ js/script.js
@@ -6,6 +6,24 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
+    selectRandomImage(images);
     console.log(images);
-}
-getImage();
+};
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddess = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddess;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function() {
+    getImage();
+});
